5db482462c6d983a810c39620ab43ea03bff9c24
LEONARDO HENRIQUE MAZZA <1090482311021@fatec-srv.local>
2024-08-15 11:02:25 -0300

    (15/08) Template strings, propriedades abreviadas e parâmetros predefinidos

--- 02-operador-ternario.js
+++ 02-operador-ternario.js
@@ -13,3 +13,21 @@ console.log(media, '=>', resultado)
 resultado = media >= 6 ? 'APROVADO' : 'REPROVADO'
 
 console.log(media, '=>', resultado)
+
+let user = 'guest', msg
+
+/* 
+    Quando há apenas uma linha após um if, um else, um while, etc.,
+    podemos omitir as chaves
+*/
+if(user === 'admin') msg = 'Seja bem-vindo'
+else msg = 'Acesso negado'
+
+// Duas variáveis avulsas, observe a saída no console
+console.log(user, msg)
+
+// Equivale ao if..else acima, usando operador ternário
+msg = user === 'admin' ? 'Seja bem-vindo' : 'Acesso negado'
+
+// Veja que, agora, as variáveis estão entre {}. Observe a diferença na saída do terminal
+console.log({user, msg})
