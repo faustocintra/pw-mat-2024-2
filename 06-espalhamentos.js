d1840f24002809186f509a2fe19a415e06a2d7f7
rafaela santos <rafaelasantosdeveloper@gmail.com>
2024-08-16 10:55:04 -0300

    (16/08) espalhamento e desestruturaçaõ

--- 06-espalhamentos.js
+++ 06-espalhamentos.js
@@ -13,6 +13,76 @@ maximo = Math.max(nums) //NÃO FUNCIONA
 
 console.log({ minimo, maximo })
 
+/**
+ A sintaxe de espalhamento ou spreading (representada por ...
+ antes do nome da variavel) é capaz de "desempacotar" um vetor 
+ em uma serie de valores avulsos
+ */
+
+minimo = Math.min(...nums)
+maximo = Math.max(...nums)
+
+console.log({ minimo, maximo })
+
+/**********************************************************/
+
+const carro1 = {
+    modelo: 'Fiorino',
+    marca: 'Fiat',
+    ano: 2015,
+    cor: 'Prata'
+}
+
+// "Copiando" carro1 para carro2
+//const carro2 = carro1   --> NÃO FUNCIONA
+
+//forçando a cópia de uma objeto usando a sintaxe de espalhamento 
+const carro2 = { ...carro1 }
+
+//Mudando o valor das propriedades de carro2
+
+carro2.modelo = 'Fusca'
+carro2.marca = 'Volkswagen'
+carro2.cor = 'Preto'
+carro2.ano = 2000
+
+//Exibindo ambos os carros 
+console.log({ carro1, carro2 })
+
+
+/****************************************/
+
+//PROBLEMA : JUNTAR DOIS OU MAIS VETORES EM UM TERCEIRO VETOR 
+
+const frutas = ['maça', 'banana', 'laranja']
+const verduras = ['alface', 'couve', 'rúcula']
+
+//juntando os dois vetores usando JS "classico" (anterior a 2015)
+//const hortifruti = frutas.concat(verduras)
+
+//usando espalhamento para unir vetores
+const hortifruti = [...frutas, ...verduras]
+
+console.log(hortifruti)
+
+
+/*********************************************/
+
+//PROBLEMA: escrever uma função que recebe um numero arbitrario de parametros
+
+function soma(...nums) {
+    //nums é recebido na função como um vetor 
+    console.log('Parametros recebidos:', nums)
+    let resultado = 0
+    for (let n of nums) resultado += n
+    return resultado
+
+}
+console.log('Soma de 7 números:', soma(1, 2, 3, 4, 5, 6, 7))
+console.log('Soma de 12 números:', soma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+
+
+
 
 
 
---
+++ 07-desestruturacao.js
@@ -0,0 +1,74 @@
+/*
+Desestruturação é a operação pela qual é possivel extrair valores de vetores
+e objetos, atribuindo-os a variaveis avulsas
+ */
+
+//1) Desestruturação de vetor 
+const carros = ['fusca', 'chevette', 'opala']
+
+//desestruturação 
+const [carro1, carro2, carro3] = carros
+
+/*Fazendo o mesmo sem desestruturação 
+const carro1 = carros[0]
+const carro2 = carros[1]
+const carro3 = carros[2]
+*/
+
+console.log({ carro1, carro2, carro3 })
+
+//desestruturação parcial: 1° e 2° valores
+const [a1, a2] = carros
+console.log({ a1, a2 })
+
+//desestruturação parcial: 1° e 3° valores
+const [b1, , b3] = carros
+console.log({ b1, b3 })
+
+//desestruturação parcial: 2° e 3° valores
+const [, c2, c3] = carros
+console.log({ c2, c3 })
+
+/***************************************************/
+//traço separador
+console.log('-'.repeat(80))
+
+//PROBLEMA: troca de valores de variaveis entre si: SWAP
+let x = 10, y = 20;
+console.log({ x, y });
+
+//Modo classico de fazer SWAP(usando variavel auxiliar)
+/*
+let aux =x
+x=y
+y=aux
+*/
+
+//usando desestruturaçaõ para fazer swap(note que os pontos-e-virgula)
+//são necessarios nesta linha e na anterior
+
+[x, y] = [y, x];
+console.log({ x, y });
+
+/***************************************/
+//traços separador
+console.log('-'.repeat(80))
+
+//2) DESESTRUTURAÇÃO DE OBJETOS
+const pessoa = {
+    nome: 'Rafaela',
+    sexo: 'F',
+    dataNasc: '2005-06-18',
+    email: 'rafinha@gmail.com'
+}
+
+/*
+Na desestruturação de objetos, as variaveis avulsas:
+~> DEVEM ter o MESMO NOME das propriedades do objeto
+~> Podem ser especificadas em qualquer ordem 
+~> Pode ser feita a desestruturação parcial
+ */
+
+const { sexo, nome, email } = pessoa
+console.log({ nome, sexo, email })
+
