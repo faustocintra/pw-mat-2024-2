6be997da218d9da75ccc709496f64dad4a465640
Fausto G. Cintra <professor@faustocintra.com.br>
2024-09-27 11:06:43 -0300

    (27/09) Exercícios 01 e 02

--- hooks/src/exercicios/02.jsx
+++ hooks/src/exercicios/02.jsx
@@ -3,11 +3,21 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 inicialize o estado como o valor do localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(
+    window.localStorage.getItem('name') ?? initialName
+  )
+
+  const [count, setCount] = React.useState(0)
 
   // 🐨 Aqui é onde usamos `React.useEffect`.
   // A função deve armazenar `name` no localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+    console.count('Atualizou')
+  }, [name])
+  // [name] é vetor de dependências
+  // Este useEffect() somente será executado quando o valor da variável name mudar
 
   function handleChange(event) {
     setName(event.target.value)
@@ -17,14 +27,16 @@ function Greeting({initialName = ''}) {
       <form>
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
+        
       </form>
       {name ? <strong>Olá {name}</strong> : 'Por favor, informe seu nome'}
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
     </div>
   )
 }
 
-function App() {
-  return <Greeting />
+function Exercicio02() {
+  return <Greeting initialName="Felisberto" />
 }
 
-export default App
+export default Exercicio02
