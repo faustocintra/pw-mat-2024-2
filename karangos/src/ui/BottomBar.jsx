f96430e0b192ecb9b864f8826b52a970922a629f
Murilo Sanches <zmurilo2010@gmail.com>
2024-10-17 11:22:17 -0300

    (17/10) MainMenu e BottomBar

---
+++ karangos/src/ui/BottomBar.jsx
@@ -0,0 +1,32 @@
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function BottomBar() {
+  /* sx = estilos estendidos do MUI, estende styles */
+  return (
+    <Toolbar
+      variant="dense"
+      sx={{
+        backgroundColor: "action.disabledBackground",
+        justifyContent: "center",
+        position: "fixed",
+        bottom: 0,
+        width: "100vw",
+      }}
+    >
+      <Typography
+        variant="caption"
+        gutterBottom
+        sx={{
+          display: "block",
+          "& a": {
+            color: "secondary.light",
+          },
+        }}
+      >
+        Desenvolvido e mantido com café por{" "}
+        <a href="mailto:professor@faustocintra.com.br">Prof. Fausto Cintra</a>
+      </Typography>
+    </Toolbar>
+  );
+}
