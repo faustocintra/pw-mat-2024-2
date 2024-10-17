3b2f2d0515500be7c30384246208c8a6a3b62dd2
Caio Eduardo <caioeduardo1403@gmail.com>
2024-10-17 11:21:52 -0300

    adição do menu

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -1,6 +1,6 @@
-import '@fontsource/roboto/300.css'; 
-import '@fontsource/roboto/400.css'; 
-import '@fontsource/roboto/500.css'; 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 //import './App.css'
@@ -8,16 +8,24 @@ import { BrowserRouter } from 'react-router-dom'
 
 import CssBaseline from '@mui/material/CssBaseline'
 import TopBar from './ui/TopBar'
+import BottomBar from './ui/BottomBar'
 
+import theme from './ui/theme'
+import { ThemeProvider } from '@mui/material/styles'
 
 function App() {
-  
+
   return (
     <>
-     <CssBaseline />
-     <BrowserRouter>
-        <TopBar />
-     </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> { /* Reseta o CSS */ }
+        
+        <BrowserRouter>
+          <TopBar />
+          <BottomBar />
+        </BrowserRouter>
+      
+      </ThemeProvider>
     </>
   )
 }
--- karangos/src/ui/TopBar.jsx
+++ karangos/src/ui/TopBar.jsx
@@ -2,21 +2,17 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import logo from '../assets/karangos-logo-600px.png'
+import MainMenu from './MainMenu'
 
 export default function TopBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" enableColorOnDark>
         <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
+          <MainMenu />
           <Box>
-            <img src={logo} alt="LogoTipo Karangos" style={{width: '300px'}}/>
+            <img src={logo} alt="Logotipo Karangos" style={{ width: '300px' }} />
           </Box>
         </Toolbar>
       </AppBar>
