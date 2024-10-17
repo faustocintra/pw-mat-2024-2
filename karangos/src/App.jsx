6f938960068e2855a93c2a0ef8a9eb464ed68b25
rafaela santos <rafaelasantosdeveloper@gmail.com>
2024-10-17 11:06:54 -0300

    (17/10) MainMenu e BottomBar

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -6,17 +6,25 @@ import '@fontsource/roboto/700.css';
 
 //import './App.css'
 import {BrowserRouter } from 'react-router-dom'
-
+import  CssBaseline  from '@mui/material/CssBaseline';
 import TopBar from './ui/TopBar';
-import { CssBaseline } from '@mui/material';
+import BottomBar from './ui/BottomBar';
+
+import theme from './ui/theme';
+import { ThemeProvider } from '@mui/material/styles';
+
 function App() {
 
   return (
     <>
-    <CssBaseline/> {/*Reseta o CSS */}
-     <BrowserRouter>
-     <TopBar/>
-     </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/> {/*Reseta o CSS */}
+
+      <BrowserRouter>
+        <TopBar/>
+        <BottomBar/>
+      </BrowserRouter>
+    </ThemeProvider>
     </>
   )
 }
---
+++ karangos/src/ui/BottomBar.jsx
@@ -0,0 +1,35 @@
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+
+
+export default function BottomBar(){
+    /*sx= estilos estendidos do MUI, estende styles */
+    return(
+        <Toolbar 
+        variant="dense"
+        sx={{
+            backgroundColor: 'action.disabledBackground',
+            justifyContent: 'center',
+            position: 'fixed',
+            bottom:0,
+            width: '100vw'
+        }}
+        >        
+            <Typography 
+            variant="caption" 
+            gutterBottom 
+            sx={{
+                 display: 'block',
+                 '& a': {
+                    color: 'secondary.light'
+                 }
+
+            }}
+            >
+            Desenvolvido e mantido com cafe por <a href="mailto:rafaelasantosdeveloper@gmail.com">
+            Rafaela Aparecida Dos Santos</a>
+            </Typography>     
+        </Toolbar>
+    )
+    
+}
