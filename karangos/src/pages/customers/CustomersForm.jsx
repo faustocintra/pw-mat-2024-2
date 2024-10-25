82bba6cefe54c3d8dfe2a94025d91166486d53b7
OJEVNE EISSIMELL VOIGT <1090482311037@fatec-srv.local>
2024-10-25 11:04:07 -0300

    (25/10) Término da listagem de clientes

--- karangos/src/pages/customers/CustomersForm.jsx
+++ karangos/src/pages/customers/CustomersForm.jsx
@@ -1,5 +1,33 @@
+import React from 'react'
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+
 export default function CustomersForm(){
+
+    /*
+        "id": 344,
+        "name": "Etelvina Eanes Essado",
+        "ident_document": "333.333.333-33",
+        "birth_date": "1957-7-7",
+        "street_name": "Rua do Sobe e Desce",
+        "house_number": "333",
+        "complements": null,
+        "district": "Centro",
+        "municipality": "Pequenópolis",
+        "state": "SP",
+        "phone": "(16) 3333-3333",
+        "email": "etel.eanes@gmail.com"
+    */
+
+        const [state, setState] = React.useState({
+            customer: {
+
+            }
+        })
+        const {
+            customer
+        } = state
 
     return (
 
@@ -9,6 +37,36 @@ export default function CustomersForm(){
 
             Cadastro de Clientes
         </Typography>
+        <Box className='form-fields'>
+            <form>
+                 {/*Autofocus = foco do teclado no primeiro campo*/}
+                 <TextField 
+                    variant="outlined" 
+                    name="name"
+                    label="Nome completo"
+                    fullWidth
+                    required
+                    autoFocus
+                    value={customer.name}
+                />
+
+
+                <TextField 
+                    variant="outlined" 
+                    name="ident_document"
+                    label="CPF"
+                    fullWidth
+                    required
+                    value={customer.ident_document}
+                />
+
+
+
+
+
+            </form>
+
+        </Box>
         </>
 
     )
--- karangos/src/pages/customers/CustomersList.jsx
+++ karangos/src/pages/customers/CustomersList.jsx
@@ -2,11 +2,16 @@ import Typography from '@mui/material/Typography'
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import { DataGrid } from '@mui/x-data-grid';
-import{ feedbackWait, feedbackNotify } from '../../ui/Feedback'
+import{ feedbackWait, feedbackNotify, feedbackConfirm } from '../../ui/Feedback'
 import EditIcon from '@mui/icons-material/Edit'
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
 import IconButton from '@mui/material/IconButton'
 import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+
+
 
 
 export default function CustomersList() {
@@ -25,7 +30,13 @@ export default function CustomersList() {
     {
       field: 'birth_date',
       headerName: 'Data Nasc.',
-      width: 150
+      width: 150,
+      valueGetter: (value, row) => {
+        if(value){
+          const date = new Date(value)
+          return date.toLocaleDateString('pt-BR')
+        }
+      }
     },
     {
       field: 'municipality',
@@ -53,13 +64,17 @@ export default function CustomersList() {
 
         return <>
         <Link to={'./' + params.id}>
-          <IconButton aria-label="editar">
+          <IconButton 
+          aria-label="editar"
+          >
             <EditIcon />
           </IconButton>
 
         </Link>
 
-        <IconButton aria-label="excluir">
+        <IconButton aria-label="excluir"
+        onClick= { () => handleDeleteButtonClick(params.id)}
+        >
           <DeleteForeverIcon color="error" />
         </IconButton>
 
@@ -85,7 +100,9 @@ export default function CustomersList() {
   async function loadData() {
     feedbackWait(true)
     try {
-      const response = await fetch('https://api.faustocintra.com.br/v2/customers')
+      const response = await fetch(
+        import.meta.env.VITE_API_BASE + '/customers'
+      )
       const result = await response.json()
 
       setState({ ...state, customers: result })
@@ -99,12 +116,53 @@ export default function CustomersList() {
     }
   }
 
+  async function handleDeleteButtonClick(id){
+    if (await feedbackConfirm('Deseja realmente excluir este item?')){
+      feedbackWait(true)
+    try {
+      //Envia a requisição para a exclusão
+      await fetch(
+        import.meta.env.VITE_API_BASE + `/customers/${id}`
+        )
+      {method: 'DELETE'}
+
+      //Atualiza os dados do datagrid
+      loadData()
+
+      feedbackNotify('Exclusão efetuada com sucesso !')
+    }
+    catch (error) {
+      console.log (error)
+      feedbackNotify ('ERRO: ' + error.message, 'error')
+    }
+    finally{
+      feedbackWait(false)
+    }
+   }
+  }
+
   return (
     <>
       { /* gutterBottom coloca um espaçamento extra abaixo do componente */ }
       <Typography variant="h1" gutterBottom>
         Listagem de clientes
       </Typography>
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'right', //Alinhando à direita
+        mb:2 //Margem inferior (margin-bottom)
+      }} >
+      <Link to=".new">
+        <Button variant="contained" 
+        size="large"
+        color="secondary"
+        startIcon={< AddCircleIcon /> }>
+          Novo Cliente
+        </Button>
+      </Link>
+
+      </Box>
+
       <Paper elevation={8} sx={{ height: 400, width: '100%' }}>
         <DataGrid
           rows={customers}
